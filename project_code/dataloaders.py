ab1ee22c9864e641731d5f249fc6a4098942869e
Jesper-H <43323978+Jesper-H@users.noreply.github.com>
2025-09-16 16:11:14 +0200

    Delete project_code directory
    Folder name changed. git no like it

--- project_code/dataloaders.py
+++
@@ -1,8 +0,0 @@
-def open_data(path:str) -> 'pd.DataFrame':
-    "Maps file extention to loader and calls it"
-    ext2loader = {
-        '.pickle':lambda p: pd.read_pickle(p),
-        '.csv':pd.read_csv,
-    }
-    _, extention = os.path.splitext(path)
-    return ext2loader[extention](path)
